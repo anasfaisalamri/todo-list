e2fbf394468683b7f30744bb92c89cb0e8aae590
anasfaisalamri <anasfasialamri@gmail.com>
2022-06-21 14:52:35 +0700

    add fn for add todos

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import TodoForm from "./TodoForm";
 import TodoList from "./TodoList";
 
 function Todo() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (todo) => {
+    const newTodos = [todo, ...todos];
+
+    setTodos(newTodos);
+  };
+
   return (
     <div>
       <h1>Todo List</h1>
-      <TodoForm />
-      <TodoList />
+      <TodoForm onSubmit={addTodo} />
+      <TodoList todos={todos} />
     </div>
   );
 }
