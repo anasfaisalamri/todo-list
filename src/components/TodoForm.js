3317dd589a5be3772c3cabc2ea364657a131af9c
anasfaisalamri <anasfasialamri@gmail.com>
2022-06-21 15:40:44 +0700

    add effect focus to input, condition input when editText

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,34 +1,60 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 function TodoForm(props) {
-  const [inputTodo, setInputTodo] = useState("");
+  const [todoInput, setTodoInput] = useState(
+    props.editText ? props.editText.value : ""
+  );
+
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    inputRef.current.focus();
+  });
 
   const handleSubmit = (event) => {
     event.preventDefault();
 
     props.onSubmit({
       id: Math.floor(Math.random() * 1000),
-      text: inputTodo,
+      text: todoInput,
       complete: false,
     });
 
-    setInputTodo("");
+    setTodoInput("");
   };
 
   const handleChange = (event) => {
-    setInputTodo(event.target.value);
+    setTodoInput(event.target.value);
   };
 
   return (
     <form className="todo-form" onSubmit={handleSubmit}>
-      <input
-        type="text"
-        className="todo-input"
-        placeholder="Create Todo"
-        value={inputTodo}
-        onChange={handleChange}
-      />
-      <button className="todo-button">Create</button>
+      {props.editText ? (
+        <>
+          <input
+            type="text"
+            name="text"
+            className="todo-input edit"
+            placeholder="Update Todo"
+            value={todoInput}
+            onChange={handleChange}
+            ref={inputRef}
+          />
+          <button className="todo-button edit">Update</button>
+        </>
+      ) : (
+        <>
+          <input
+            type="text"
+            className="todo-input"
+            placeholder="Create Todo"
+            value={todoInput}
+            onChange={handleChange}
+            ref={inputRef}
+          />
+          <button className="todo-button">Create</button>
+        </>
+      )}
     </form>
   );
 }
