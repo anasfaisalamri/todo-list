f847d60f299be85da47956aa5aa013dddb7c377e
anasfaisalamri <anasfasialamri@gmail.com>
2022-06-21 14:40:09 +0700

    create form structor

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 function TodoForm() {
-  return <div>TodoForm</div>;
+  const [inputTodo, setInputTodo] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    setInputTodo("");
+  };
+
+  const handleChange = (event) => {
+    setInputTodo(event.target.value);
+  };
+
+  return (
+    <form className="todo-form" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="todo-input"
+        value={inputTodo}
+        onChange={handleChange}
+      />
+      <button>Create</button>
+    </form>
+  );
 }
 
 export default TodoForm;
