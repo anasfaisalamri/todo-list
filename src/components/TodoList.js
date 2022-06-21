eb894b6b158f361fe9ec9ccd1a50b492b60c47f9
anasfaisalamri <anasfasialamri@gmail.com>
2022-06-21 14:53:29 +0700

    add structur todo-list

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,7 +1,36 @@
 import React from "react";
+import { CgCheckR, CgCloseR } from "react-icons/cg";
+import { FiEdit } from "react-icons/fi";
 
-function TodoList() {
-  return <div>TodoList</div>;
+function TodoList(props) {
+  return (
+    <div className="todo-list">
+      {props.todos.map((todo, index) => (
+        <div
+          className={todo.isComplete ? "todo-row complete" : "todo-row"}
+          key={index}
+        >
+          <div key={todo.id}>
+            <div>{todo.text}</div>
+          </div>
+          <div className="icons">
+            <CgCheckR
+              onClick={() => props.completeTodo(todo.id)}
+              className="complete-icon"
+            />
+            <FiEdit
+              onClick={() => setEdit({ id: todo.id, value: todo.text })}
+              className="edit-icon"
+            />
+            <CgCloseR
+              onClick={() => props.removeTodo(todo.id)}
+              className="delete-icon"
+            />
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default TodoList;
