6614488df40a77cf66bfac8ec49438ffce7536d1
anasfaisalamri <anasfasialamri@gmail.com>
2022-06-21 14:39:24 +0700

    importing components

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,9 +1,13 @@
 import React from "react";
+import TodoForm from "./TodoForm";
+import TodoList from "./TodoList";
 
 function Todo() {
   return (
     <div>
       <h1>Todo List</h1>
+      <TodoForm />
+      <TodoList />
     </div>
   );
 }
